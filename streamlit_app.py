2deb58e875f8bac1277eefcdf6c7ae9e2ccc2e72
Natalia <natkro15@st.amu.edu.pl>
2026-04-26 12:40:02 +0200

    add upload file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import os
 
 st.set_page_config(layout="wide", page_title="Gemini chatbot app")
 st.title("Gemini chatbot app")
+uploaded_file = st.file_uploader("Dodaj plik tekstowy", type=("txt", "md"))
 
 # api_key, base_url = os.environ["API_KEY"], os.environ["BASE_URL"]
 api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
@@ -19,6 +20,10 @@ if prompt := st.chat_input():
     if not api_key:
         st.info("Invalid API key.")
         st.stop()
+    if uploaded_file is not None:
+        content = uploaded_file.read().decode("utf-8")
+        prompt = f"Treść pliku:\n{content}\n\nPytanie: {prompt}"
+        
     client = OpenAI(api_key=api_key, base_url=base_url)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
@@ -30,3 +35,5 @@ if prompt := st.chat_input():
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
+
+    
